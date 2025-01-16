58a1b4bb912f1f7bb1a0925b853e5a1990864835
Cjungwo <jwoo516@naver.com>
2025-01-16 21:35:10 +1100

    start ui dev and add dev-nav for ui test

---
+++ src/components/__dev__/dev-nav.tsx
@@ -0,0 +1,25 @@
+import Link from 'next/link';
+
+interface LinkItem {
+    tag: string;
+    href: string;
+}
+
+const links: LinkItem[] = [
+    {
+        tag: 'Content Detail',
+        href: '/contents/7d9b2356-0dfe-416c-819b-ec5d1ae6b74a',
+    },
+    {
+        tag: 'Contents',
+        href: '/contents',
+    },
+];
+
+export const DevNav = () => {
+   return <div className='fixed right-10 bottom-10'>{links.map((c) => (
+    <div key={c.tag}>
+        <Link href={c.href}>{c.tag}</Link>
+    </div>
+   ))}</div>;
+};
