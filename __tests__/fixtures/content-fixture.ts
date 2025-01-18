f10e8caef41a4c767a406abbdf20914a1709af65
Cjungwo <jwoo516@naver.com>
2025-01-19 09:52:50 +1100

    #5-add entity fixtures for test

---
+++ __tests__/fixtures/content-fixture.ts
@@ -0,0 +1,116 @@
+import { Content } from "@/domains/content/content.entity";
+
+export const contentFixtures: Content[] = [
+    {
+        id: "cb1a1f08-f2e8-4c26-a85b-cd8fbc7bd531",
+        createAt: new Date("2024-06-04T01:51:03.348Z"), 
+        title: "Go Tell It on the Mountain", 
+        body: "sunbeam worthless where brr hm unbalance father collaboration afore", 
+        thumbnail: "/vercel.svg", 
+        authorId: "1f61cde4-2db6-4424-9d2d-5b6ddb8ad351"
+    },
+    {
+        id: "e0428498-420c-49fe-a72f-5ae2caa6d18b", 
+        createAt: new Date("2024-04-22T22:10:52.939Z"), 
+        title: "Les Miserables", 
+        body: "valiantly clinking weary indeed delight exactly behold", 
+        thumbnail: "/globe.svg", 
+        authorId: "a5738dab-0622-40d5-a21c-6ee0a7623e35"
+    },
+    { 
+        id: "5330a825-d893-4b83-bf0c-3678f8053bbb", 
+        createAt: new Date("2024-06-21T17:38:44.544Z"), 
+        title: "One Flew Over the Cuckoo's Nest", 
+        body: "after among bootleg seriously handy abnormally but quizzically toward banish queasily incidentally failing synthesise smug above diligently", 
+        thumbnail: "/window.svg", 
+        authorId: "e69590a3-df72-41fa-a856-799f8c9a08f7"
+    },
+    { 
+        id: "a168f64a-bb0c-4fd6-80ff-910f8ee1eae4", 
+        createAt: new Date("2024-10-22T06:23:44.338Z"), 
+        title: "The Canterbury Tales", 
+        body: "whitewash pish label consequently jealously ribbon clavicle yuck yuppify", 
+        thumbnail: "/vercel.svg", 
+        authorId: "b1e84f97-ca47-40d2-b6d9-917eab7e527f"
+    }, 
+    {
+        id: "d0ee7f9e-0230-4f67-830d-44cea9f88ab8", 
+        createAt: new Date("2024-05-06T11:11:22.412Z"), 
+        title: "A Handful of Dust", 
+        body: "splash admired forsaken once optimal eek inasmuch or almost frankly construe outside regarding abscond snowplow whenever wherever ha", 
+        thumbnail: "/globe.svg", 
+        authorId: "127d1edc-a6ef-4cd4-a10b-6080dce5600b"
+    },
+    { 
+        id: "3d94f541-ed3f-46ba-b88c-b9479a59ce0b", 
+        createAt: new Date("2024-12-24T20:05:53.570Z"), 
+        title: "The Ambassadors", 
+        body: "wholly cheetah planula surprise soup knowledgeably knowledgeably ew capitalize refer that curl meh boohoo geez", 
+        thumbnail: "/vercel.svg", 
+        authorId: "b1446551-7b0c-47a8-826d-b4c2fb14478b"
+    }, 
+    {
+        id: "21015df5-df61-4b7f-81a2-3e94390c2e99", 
+        createAt: new Date("2024-08-07T12:23:59.161Z"), 
+        title: "The Three Musketeers", 
+        body: "dish quadruple than why qua exploration handful onto um blaspheme which", 
+        thumbnail: "/window.svg", 
+        authorId: "f9d61867-f370-4382-a0ab-aacd567cbd1b"
+    },
+    { 
+        id: "43cfea7e-d75a-44ee-b6f4-24a5365a55e5", 
+        createAt: new Date("2024-02-19T18:40:08.822Z"), 
+        title: "Oliver Twist", 
+        body: "shady except blah sometimes beyond yuck", 
+        thumbnail: "/vercel.svg", 
+        authorId: "958a023e-7d47-4be6-94da-b8ba2400c0ba"
+    }, 
+    {
+        id: "987081b1-30bf-4b7b-834d-1fd6a2cfa305", 
+        createAt: new Date("2024-08-07T11:06:29.427Z"), 
+        title: "The Pillars of the Earth", 
+        body: "gently viciously ack present beyond vice approximate where lest across absent psst weary physically actually sardonic bare", 
+        thumbnail: "/file.svg", 
+        authorId: "75ca8541-75c2-47aa-9ab1-a8f744baa0fa"
+    }, 
+    {
+        id: "95b6b9b1-b4e8-452a-bdce-9512b3ea1e69", 
+        createAt: new Date("2024-12-12T18:47:11.174Z"), 
+        title: "Scoop", 
+        body: "physically haversack mozzarella wonderfully who", 
+        thumbnail: "/file.svg", 
+        authorId: "61690444-983b-4efd-8b19-31bc6103c710"
+    }, 
+    {
+        id: "8fba416f-08b9-4c2b-848a-934e324c8c80", 
+        createAt: new Date("2024-05-20T00:43:20.021Z"), 
+        title: "The Lion, the Witch, and the Wardrobe", 
+        body: "briskly whole separately scotch transparency ready an after sparkling bogus stable outlying conservative for before", 
+        thumbnail: "/window.svg", 
+        authorId: "f8257074-ecc0-4108-a84e-81e9113eccea"
+    }, 
+    {
+        id: "589fb2ba-5d48-44ab-9e99-dd644faef2be", 
+        createAt: new Date("2024-06-28T06:40:41.404Z"), 
+        title: "The Canterbury Tales", 
+        body: "meanwhile faithfully shakily bobble how gymnast even hyphenation", 
+        thumbnail: "/window.svg", 
+        authorId: "7f3d241d-e2a5-42fa-9208-15cb9623c3da"
+    }, 
+    {
+        id: "6d258ada-2f05-4ae5-8e15-a25003c5da81", 
+        createAt: new Date("2024-09-05T05:49:38.832Z"), 
+        title: "The Strange Case of Dr. Jekyll and Mr. Hyde", 
+        body: "soybean contractor gah ultimately describe preregister understanding blaring geez ew whoever obedience till", 
+        thumbnail: "/vercel.svg", 
+        authorId: "f1314b5a-f5bc-429c-945a-efa18e0439f0"
+    }, 
+    {
+        id: "8aebce26-8406-442c-889d-28ff27f856a3", 
+        createAt: new Date("2024-10-02T16:26:33.819Z"), 
+        title: "The Canterbury Tales", 
+        body: "gymnast during whether with ick ugh untimely prioritize litter bleakly", 
+        thumbnail: "/vercel.svg", 
+        authorId: "89ea516d-4f37-46f6-a642-7f2ee7ee0088"
+    }
+]
