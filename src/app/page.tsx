dbe26beb38cb15959faaa4cfc8718ac9a0a892ea
Cjungwo <jwoo516@naver.com>
2025-01-19 09:48:49 +1100

    #5-add go link in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <div>
       <h1 className="mt-20 text-4xl font-bold text-center leading-snug">
         <div>Front-End</div>
-        <div className="text-6xl text-green-400">TDD Completed!!</div>
+        <div className="text-6xl text-green-400 mb-8">TDD Completed!!</div>
+        <Link href='/contents'>&lt;&lt;GO&gt;&gt;</Link>
       </h1>
     </div>
   );
