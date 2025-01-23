da337c394f9b65ad1838f0d0dde0811802b14432
Cjungwo <jwoo516@naver.com>
2025-01-24 10:56:13 +1100

    #5-add mock server in nextjs

--- src/effects/main/content-api.effect.ts
+++ src/effects/main/content-api.effect.ts
@@ -2,15 +2,16 @@ import { ContentView } from "@/domains/content/content.type";
 import { jsonDateParser } from "json-date-parser";
 
 export const contentApi = {
-    async findOne(id: string): Promise<{
-        data: {
-            content: ContentView;
-        };
-        status: 200;
-      } 
-    | {
-        status: 404;
-      }
+    async findOne(id: string): Promise<
+      | {
+          data: {
+              content: ContentView;
+          };
+          status: 200;
+        } 
+      | {
+          status: 404;
+        }
     > {
         const url = process.env.NEXT_PUBLIC_API_BASE_URL + `/contents/${id}`;
         console.log("DEBUG: ", url);
