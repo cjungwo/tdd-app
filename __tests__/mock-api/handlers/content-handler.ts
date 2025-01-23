1b86fa4e58a0b89ef09ff19ba24afeddfdd681e2
Cjungwo <jwoo516@naver.com>
2025-01-24 10:29:45 +1100

    #5-add content detail api and test

--- __tests__/mock-api/handlers/content-handler.ts
+++ __tests__/mock-api/handlers/content-handler.ts
@@ -1,11 +1,46 @@
+import { ContentView } from '@/domains/content/content.type';
+import { contentFixtures } from '@__tests__/fixtures/content-fixture';
+import { userFixtures } from '@__tests__/fixtures/user-fixture';
 import { http, HttpResponse } from 'msw';
+import { omit } from 'radashi';
 
 export const contentHandler = [
-    http.get('http://localhost:4000/contents', () => {
+    http.get(process.env.NEXT_PUBLIC_API_BASE_URL + '/contents', () => {
         return HttpResponse.json({
             id: 'dkdkwo2t933gj2g',
             firstName: 'Chris',
             lastName: 'Choi'
+        });
+    }),
+
+    http.get(process.env.NEXT_PUBLIC_API_BASE_URL + '/contents/:id', ({ params }) => {
+        const { id } = params;
+
+        if (typeof id !== 'string') return HttpResponse.json({
+            status: 400, // 요청 폼 오류
+        });
+
+        const found = contentFixtures.find((c) => c.id === id);
+
+        if (!found) return HttpResponse.json({
+            status: 404, // not found
+        });
+        
+        const author = userFixtures.find((c) => c.id === found.authorId);
+         
+        if (!author) return HttpResponse.json({
+            status: 404, // author not found
+            message: 'Author not found',
+        });
+
+        const content: ContentView = {
+            ...omit(found, ['authorId']),
+            author,
+        };
+
+        return HttpResponse.json({
+            data: { content },
+            status: 200
         });
     }),
 ];
--- src/app/contents/[id]/page.tsx
+++ src/app/contents/[id]/page.tsx
@@ -3,13 +3,34 @@ import { ContentDetailCommentSection } from "@/components/organisms/content-deta
 import { ContentDetailMain } from "@/components/organisms/content-detail-main";
 import { Footer } from "@/components/organisms/footer";
 import { Header } from "@/components/organisms/header";
+import { getAuthUser } from "@/effects/authorization";
+import { contentApi } from "@/effects/main/content-api.effect";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
-export default function ContentDetail() {
+export default async function ContentDetail({
+   params,
+}: {
+   params: Promise<{ id: string }>
+}) {
+   const user = await getAuthUser(cookies);
+   const { id } = await params;
+   console.log("DEBUG: ", id);
+   
+   const response = await contentApi.findOne(id);
+
+   if (response.status !== 200) {
+      redirect('/contents');
+   }
+
    return <>
-   <Header />
-   <ContentDetailMain />
-   <ContentDetailAuthorAside />
-   <ContentDetailCommentSection />
-   <Footer />
+      <Header 
+         contentAuthorNickname={response.data.content.author.nickname}
+         user={user} 
+      />
+      <ContentDetailMain />
+      <ContentDetailAuthorAside />
+      <ContentDetailCommentSection />
+      <Footer />
    </>;
 };
