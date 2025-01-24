f661e063e9bdfd2653911827f4569bafd2650dd4
Cjungwo <jwoo516@naver.com>
2025-01-24 16:12:25 +1100

    #6-impl content detail page

--- src/components/organisms/content-detail-comment-section/index.tsx
+++ src/components/organisms/content-detail-comment-section/index.tsx
@@ -1,18 +1,35 @@
+'use client';
+
 import { layoutStyles } from '@/styles/layout-styles';
 import clsx from 'clsx';
+import { useRouter } from 'next/navigation';
 
 interface Props {
    className?: string;
+   isAuthorized: boolean;
 }
 
 export const ContentDetailCommentSection = (props: Props) => {
-   return <section className={clsx(layoutStyles.mx, 'mt-8 mb-12', props.className)}>
+   const router = useRouter();
+
+   const onClickTextarea = () => {
+      if (!props.isAuthorized) {
+         router.push('/users/sign-in');
+         return;
+      }
+   }
+
+   return <section 
+      className={clsx(layoutStyles.mx, 'mt-8 mb-12', props.className)}
+      data-testid='comment-section'
+   >
       <form action="">
          <textarea 
             name="comment" 
             id="comment" 
             className='w-full bg-neutral-800 rounded px-4 py-4 resize-none outline-none' 
             placeholder='Write a comment'
+            onClick={onClickTextarea}
          />
          <div className='flex justify-end'>
             <button className='bg-green-300 text-black rounded px-4 py-2'>Send</button>
--- src/components/organisms/content-detail-main/index.tsx
+++ src/components/organisms/content-detail-main/index.tsx
@@ -1,3 +1,4 @@
+import { ContentView } from '@/domains/content/content.type';
 import { localizeDate } from '@/libs/sub-string';
 import { layoutStyles } from '@/styles/layout-styles';
 import { interpunct } from '@/utils/string';
@@ -6,31 +7,32 @@ import Link from 'next/link';
 
 interface Props {
    className?: string;
+   content: ContentView;
 }
 
 export const ContentDetailMain = (props: Props) => {
    return (
    <main className={clsx('mt-8', layoutStyles.px, props.className)}>
       <header>
-         <h1 className='text-4xl font-bold leading-normal'>{'title'}</h1>
+         <h1 className='text-4xl font-bold leading-normal'>{props.content.title}</h1>
          <div>
-            <span>{'John'}</span>
+            <span>{props.content.author.nickname}</span>
             <span>
                {' '}
                {interpunct}
                {' '}
             </span>
-            <span>{localizeDate(new Date('2025-01-16T00:00:00'))}</span>
+            <span>{localizeDate(props.content.createAt)}</span>
          </div>
          { 
          true && <div className='flex justify-end'>
-            <Link href={`/contents/${'id'}/update`}>Update</Link>
+            <Link href={`/contents/${props.content.id}/update`}>Update</Link>
             <button>Delete</button>
          </div> 
          }
       </header>
       <div>
-         {'body'}
+         {props.content.body}
       </div>
    </main>
    );
