7bc479ed487ac90032baf14af855d22ee3fc5ad3
Cjungwo <jwoo516@naver.com>
2025-01-30 13:16:40 +1100

    #7-update generate-contents and content-fixtures for test of content list

--- __tests__/fixtures/content-fixture.ts
+++ __tests__/fixtures/content-fixture.ts
@@ -1,6 +1,14 @@
 import { Content } from "@/domains/content/content.entity";
 
 export const contentFixtures: Content[] = [
+    { 
+        id: "3d94f541-ed3f-46ba-b88c-b9479a59ce0b", 
+        createAt: new Date("2024-12-24T20:05:53.570Z"), 
+        title: "The Ambassadors", 
+        body: "wholly cheetah planula surprise soup knowledgeably knowledgeably ew capitalize refer that curl meh boohoo geez", 
+        thumbnail: "/vercel.svg", 
+        authorId: "1f61cde4-2db6-4424-9d2d-5b6ddb8ad351"
+    },
     {
         id: "7d9b2356-0dfe-416c-819b-ec5d1ae6b74a",
         createAt: new Date("2024-06-04T01:51:03.348Z"), 
@@ -12,10 +20,10 @@ export const contentFixtures: Content[] = [
     {
         id: "e0428498-420c-49fe-a72f-5ae2caa6d18b", 
         createAt: new Date("2024-04-22T22:10:52.939Z"), 
-        title: "Les Miserables", 
+        title: "000000000Les Miserables", 
         body: "valiantly clinking weary indeed delight exactly behold", 
         thumbnail: "/globe.svg", 
-        authorId: "a5738dab-0622-40d5-a21c-6ee0a7623e35"
+        authorId: "1f61cde4-2db6-4424-9d2d-5b6ddb8ad351"
     },
     { 
         id: "5330a825-d893-4b83-bf0c-3678f8053bbb", 
@@ -23,7 +31,7 @@ export const contentFixtures: Content[] = [
         title: "One Flew Over the Cuckoo's Nest", 
         body: "after among bootleg seriously handy abnormally but quizzically toward banish queasily incidentally failing synthesise smug above diligently", 
         thumbnail: "/window.svg", 
-        authorId: "e69590a3-df72-41fa-a856-799f8c9a08f7"
+        authorId: "1f61cde4-2db6-4424-9d2d-5b6ddb8ad351"
     },
     { 
         id: "a168f64a-bb0c-4fd6-80ff-910f8ee1eae4", 
@@ -31,7 +39,7 @@ export const contentFixtures: Content[] = [
         title: "The Canterbury Tales", 
         body: "whitewash pish label consequently jealously ribbon clavicle yuck yuppify", 
         thumbnail: "/vercel.svg", 
-        authorId: "b1e84f97-ca47-40d2-b6d9-917eab7e527f"
+        authorId: "1f61cde4-2db6-4424-9d2d-5b6ddb8ad351"
     }, 
     {
         id: "d0ee7f9e-0230-4f67-830d-44cea9f88ab8", 
@@ -39,15 +47,7 @@ export const contentFixtures: Content[] = [
         title: "A Handful of Dust", 
         body: "splash admired forsaken once optimal eek inasmuch or almost frankly construe outside regarding abscond snowplow whenever wherever ha", 
         thumbnail: "/globe.svg", 
-        authorId: "127d1edc-a6ef-4cd4-a10b-6080dce5600b"
-    },
-    { 
-        id: "3d94f541-ed3f-46ba-b88c-b9479a59ce0b", 
-        createAt: new Date("2024-12-24T20:05:53.570Z"), 
-        title: "The Ambassadors", 
-        body: "wholly cheetah planula surprise soup knowledgeably knowledgeably ew capitalize refer that curl meh boohoo geez", 
-        thumbnail: "/vercel.svg", 
-        authorId: "b1446551-7b0c-47a8-826d-b4c2fb14478b"
+        authorId: "1f61cde4-2db6-4424-9d2d-5b6ddb8ad351"
     }, 
     {
         id: "21015df5-df61-4b7f-81a2-3e94390c2e99", 
@@ -55,7 +55,7 @@ export const contentFixtures: Content[] = [
         title: "The Three Musketeers", 
         body: "dish quadruple than why qua exploration handful onto um blaspheme which", 
         thumbnail: "/window.svg", 
-        authorId: "f9d61867-f370-4382-a0ab-aacd567cbd1b"
+        authorId: "1f61cde4-2db6-4424-9d2d-5b6ddb8ad351"
     },
     { 
         id: "43cfea7e-d75a-44ee-b6f4-24a5365a55e5", 
@@ -63,7 +63,7 @@ export const contentFixtures: Content[] = [
         title: "Oliver Twist", 
         body: "shady except blah sometimes beyond yuck", 
         thumbnail: "/vercel.svg", 
-        authorId: "958a023e-7d47-4be6-94da-b8ba2400c0ba"
+        authorId: "1f61cde4-2db6-4424-9d2d-5b6ddb8ad351"
     }, 
     {
         id: "987081b1-30bf-4b7b-834d-1fd6a2cfa305", 
@@ -71,7 +71,7 @@ export const contentFixtures: Content[] = [
         title: "The Pillars of the Earth", 
         body: "gently viciously ack present beyond vice approximate where lest across absent psst weary physically actually sardonic bare", 
         thumbnail: "/file.svg", 
-        authorId: "75ca8541-75c2-47aa-9ab1-a8f744baa0fa"
+        authorId: "1f61cde4-2db6-4424-9d2d-5b6ddb8ad351"
     }, 
     {
         id: "95b6b9b1-b4e8-452a-bdce-9512b3ea1e69", 
@@ -79,7 +79,7 @@ export const contentFixtures: Content[] = [
         title: "Scoop", 
         body: "physically haversack mozzarella wonderfully who", 
         thumbnail: "/file.svg", 
-        authorId: "61690444-983b-4efd-8b19-31bc6103c710"
+        authorId: "1f61cde4-2db6-4424-9d2d-5b6ddb8ad351"
     }, 
     {
         id: "8fba416f-08b9-4c2b-848a-934e324c8c80", 
@@ -87,7 +87,7 @@ export const contentFixtures: Content[] = [
         title: "The Lion, the Witch, and the Wardrobe", 
         body: "briskly whole separately scotch transparency ready an after sparkling bogus stable outlying conservative for before", 
         thumbnail: "/window.svg", 
-        authorId: "f8257074-ecc0-4108-a84e-81e9113eccea"
+        authorId: "1f61cde4-2db6-4424-9d2d-5b6ddb8ad351"
     }, 
     {
         id: "589fb2ba-5d48-44ab-9e99-dd644faef2be", 
@@ -95,7 +95,7 @@ export const contentFixtures: Content[] = [
         title: "The Canterbury Tales", 
         body: "meanwhile faithfully shakily bobble how gymnast even hyphenation", 
         thumbnail: "/window.svg", 
-        authorId: "7f3d241d-e2a5-42fa-9208-15cb9623c3da"
+        authorId: "1f61cde4-2db6-4424-9d2d-5b6ddb8ad351"
     }, 
     {
         id: "6d258ada-2f05-4ae5-8e15-a25003c5da81", 
@@ -103,7 +103,7 @@ export const contentFixtures: Content[] = [
         title: "The Strange Case of Dr. Jekyll and Mr. Hyde", 
         body: "soybean contractor gah ultimately describe preregister understanding blaring geez ew whoever obedience till", 
         thumbnail: "/vercel.svg", 
-        authorId: "f1314b5a-f5bc-429c-945a-efa18e0439f0"
+        authorId: "1f61cde4-2db6-4424-9d2d-5b6ddb8ad351"
     }, 
     {
         id: "8aebce26-8406-442c-889d-28ff27f856a3", 
@@ -111,6 +111,6 @@ export const contentFixtures: Content[] = [
         title: "The Canterbury Tales", 
         body: "gymnast during whether with ick ugh untimely prioritize litter bleakly", 
         thumbnail: "/vercel.svg", 
-        authorId: "89ea516d-4f37-46f6-a642-7f2ee7ee0088"
+        authorId: "1f61cde4-2db6-4424-9d2d-5b6ddb8ad351"
     }
 ]
