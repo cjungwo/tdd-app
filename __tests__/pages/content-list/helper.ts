c6a0079fc5db362dce4dcde1df7e473300616ca8
Cjungwo <jwoo516@naver.com>
2025-01-30 13:21:12 +1100

    #7-add playwright test for content-list

---
+++ __tests__/pages/content-list/helper.ts
@@ -0,0 +1,27 @@
+import { BaseHelper } from '@__tests__/playwright/base-helper';
+import { BrowserContext, Locator, Page } from '@playwright/test';
+
+export class Helper extends BaseHelper {
+    url = '/contents';
+    readonly getPagination: Locator;
+    readonly getContentItems: Locator;
+    readonly getSearchInput: Locator;    
+    readonly getSortOption: Locator;    
+
+    constructor(page: Page, context: BrowserContext) {
+        super(page, context);
+        this.getPagination = this.page.getByTestId('pagination');
+        this.getContentItems = this.page.getByTestId('content-item');
+        this.getSearchInput = this.page.getByRole('textbox', {name: 'search'});
+        this.getSortOption = this.page.getByLabel('sort');
+    }
+   
+    async gotoTargetPage(assert: boolean) {
+        await this.page.goto(this.url);
+        if (assert) await this.strictHaveUrl(this.url);
+    }
+
+    getPageBtn(num: number) {
+        return this.getPagination.getByRole('button', { name: num + '' });
+    }
+}
