2e286ce619a791da160329e2af5d6df64a68a922
Cjungwo <jwoo516@naver.com>
2025-01-30 13:25:10 +1100

    #7-add content-api for content-list

--- src/effects/main/content-api.effect-spec.ts
+++ src/effects/main/content-api.effect-spec.ts
@@ -4,8 +4,39 @@ import { contentApi } from './content-api.effect';
 import { ContentView } from '@/domains/content/content.type';
 import { omit } from 'radashi';
 import { userFixtures } from '@__tests__/fixtures/user-fixture';
+// import { contentSortOption } from '@/domains/content/content.constant';
 
 describe('contentApi', () => {
+    test('findAll', async () => {
+        const pageNum = 1;
+        const pageTake = 12;
+        // const sort = contentSortOption.titleAsc;
+        const content = contentFixtures[3];
+        const search = content.title.slice(0, 10);
+
+        const response = await contentApi.findAll({
+            pageTake,
+            pageNum,
+            search,
+        });
+
+        console.log("DEBUG: status", response.status);
+        console.log("DEBUG: status", response.data.contents);
+        
+        expect(response.status).toEqual(200);
+        expect(response.data.contents[0].title).toEqual(content.title);
+        expect(response.data.contents).toHaveLength(1);
+    });
+
+    test('countAll', async () => {
+        const search = contentFixtures[0].title;
+
+        const response = await contentApi.countAll(search);
+
+        expect(response.status).toEqual(200);
+        expect(response.data.count).toEqual(1);
+    });
+
     test('findOne', async () => {
         const content = contentFixtures[0];
         const author = userFixtures[0];
--- src/effects/main/content-api.effect.ts
+++ src/effects/main/content-api.effect.ts
@@ -1,7 +1,53 @@
-import { ContentView } from "@/domains/content/content.type";
+import { ContentSortOption, ContentView } from "@/domains/content/content.type";
 import { jsonDateParser } from "json-date-parser";
+import qs from 'qs';
 
 export const contentApi = {
+    async findAll(query: { 
+        pageTake: number; 
+        pageNum: number; 
+        sort?: ContentSortOption;
+        search?: string;
+    }): Promise<{
+        data: {
+            contents: ContentView[];
+        };
+        status: 200;
+    }> {
+        const relative = '/contents?' + qs.stringify(query); 
+        const url = process.env.NEXT_PUBLIC_API_BASE_URL + relative;
+        console.log("DEBUG: ", url);
+        
+        const data = await fetch(url);
+        const text = await data.text();
+
+        const json = JSON.parse(text, jsonDateParser);
+
+        return json;
+    },
+    
+    async countAll(search?: string): Promise<
+      | {
+          data: {
+              count: number;
+          };
+          status: 200;
+        } 
+    > {
+        const relative = search 
+        ? `/contents/count?` + qs.stringify({ search }) 
+        : `/contents/count`;
+
+        const url = process.env.NEXT_PUBLIC_API_BASE_URL + relative;
+        console.log("DEBUG: ", url);
+        
+        const data = await fetch(url);
+        const text = await data.text();
+
+        const json = JSON.parse(text, jsonDateParser);
+
+        return json;
+    },
     async findOne(id: string): Promise<
       | {
           data: {
